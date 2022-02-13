ecd386e602bc73a925c17a043b0fa86da4c606dc
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-02-13 21:42:55 +0800

    fix

---
+++ Assets/Script/Interactive/MovingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            var target = c.gameObject.transform;
+            target.SetParent(this.transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            var target = c.gameObject.transform;
+            var original = target.GetComponent<TransformState>().OriginalParent;
+            target.SetParent(original);
+        }
+    }
+}
