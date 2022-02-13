0f24b1c0d65e52b2378066fd2deaddd0df4be7ef
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-02-13 20:51:22 +0800

    Elevator

---
+++ Assets/Script/Interactive/Dungeon/MachineController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineController : MonoBehaviour
+{
+    public Animator animator;
+    public bool IsDefault = true;
+    public bool UpToDown = false;
+    public bool OneRound = false;
+    public bool DoAction = false;
+
+    public void Start()
+    {
+        DoAction = true;
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.A))
+        {
+            if (IsDefault == true && UpToDown == false && OneRound == false && DoAction == true)
+            {
+                IsDefault = false;
+                animator.SetTrigger("DefaultToUp");
+                StartCoroutine(DelayDefaultToUp());
+                DoAction = false;
+            }
+
+            if(IsDefault == false && UpToDown == true && DoAction == true)
+            {
+                animator.SetTrigger("UpToDown");
+                StartCoroutine(DelayOneRound());
+                DoAction = false;
+            }
+
+            if(OneRound == true && UpToDown == false && DoAction == true)
+            {
+                animator.SetTrigger("DownToUp");
+                StartCoroutine(DelayDownToUp());
+                DoAction = false;
+            }
+
+            else
+            {
+                DoAction = false;
+            }
+        }
+    }
+
+
+    public IEnumerator DelayDefaultToUp()
+    {
+        yield return new WaitForSeconds(3);
+        UpToDown = true;
+        DoAction = true;
+    }
+
+    public IEnumerator DelayOneRound()
+    {
+        yield return new WaitForSeconds(3);
+        UpToDown = false;
+        OneRound = true;
+        DoAction = true;
+    }
+
+    public IEnumerator DelayDownToUp()
+    {
+        yield return new WaitForSeconds(3);
+        UpToDown = true;
+        OneRound = false;
+        DoAction = true;
+    }
+}
---
+++ Assets/Script/Interactive/Dungeon/MachineTieRodController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineTieRodController : MonoBehaviour
+{
+    public Animator animator;
+    public bool IsDefault = true;
+    public bool PullUp = false;
+    public bool DoAction = false;
+    public bool OneRound = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DoAction = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.A))
+        {
+            if(IsDefault == true && DoAction == true)
+            {
+                animator.SetTrigger("IsDefault");
+                StartCoroutine(DelayDoAction());
+                DoAction = false;
+                IsDefault = false;
+                Debug.Log("1111");
+            }
+
+            if(IsDefault == false && PullUp == true && DoAction == true && OneRound == false)
+            {
+                animator.SetTrigger("PullDown");
+                StartCoroutine(DelayPullDown());
+                DoAction = false;
+            }
+
+            if(PullUp == false && OneRound == true && DoAction == true)
+            {
+                animator.SetTrigger("PullUp");
+                StartCoroutine(DelayPullUp());
+                DoAction = false;
+            }
+
+            else
+            {
+                DoAction = false;
+            }
+        }
+    }
+
+
+    public IEnumerator DelayDoAction()
+    {
+        yield return new WaitForSeconds(3);
+        DoAction = true;
+        PullUp = true;
+    }
+
+    public IEnumerator DelayPullDown()
+    {
+        yield return new WaitForSeconds(3);
+        DoAction = true;
+        PullUp = false;
+        OneRound = true;
+    }
+
+    public IEnumerator DelayPullUp()
+    {
+        yield return new WaitForSeconds(3);
+        DoAction = true;
+        PullUp = true;
+        OneRound = false;
+    }
+}
