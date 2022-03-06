6bc8e16a74bcfc2f7a0c70fb284f81158202f243
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-03-06 22:37:10 +0800

    TransPort

--- Assets/Script/Transition/TransitionDestination.cs
+++ Assets/Script/Transition/TransitionDestination.cs
@@ -6,7 +6,8 @@ public class TransitionDestination : MonoBehaviour
 {
     public enum DestinationTag
     {
-        FOREST,VILLEGE,VILLEGEFIRE, VILLEGEFIRE_TRAPTOSAVE
+        FOREST,VILLEGE,VILLEGEFIRE, VILLEGEFIRE_TRAPTOSAVE, TRANSPORT_01,
+        TRANSPORT_01_2, TRANSPORT_02, TRANSPORT_02_2
     }
 
     public DestinationTag destinationTag;
