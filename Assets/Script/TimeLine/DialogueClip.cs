98d5d3cf57a729fa2d48c1009c45e7857912a961
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-03-11 17:26:12 +0800

    TimeLineDialogue01

---
+++ Assets/Script/TimeLine/DialogueClip.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+
+public class DialogueClip :  PlayableAsset, ITimelineClipAsset
+{
+    public DialogueBehaviour template = new DialogueBehaviour();
+
+    public Sprite picture_Asset;
+    public string str_Asset;
+    public bool hasToPause_Asset;
+
+    public ClipCaps clipCaps
+{
+    get { return ClipCaps.None; }
+}
+
+public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+{
+    var playable = ScriptPlayable<DialogueBehaviour>.Create(graph, template);
+
+        playable.GetBehaviour().dialogueCG = picture_Asset;
+        playable.GetBehaviour().dialogueLine = str_Asset;
+        playable.GetBehaviour().hasToPause = hasToPause_Asset;
+
+        return playable;
+}
+}
---
+++ Assets/Script/TimeLine/TimeLineManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class TimeLineManager : MonoBehaviour
+{
+    public static TimeLineManager ins;
+
+    public Text dialogueLineText;
+
+    public Image characterCG;
+
+    [SerializeField] PlayableDirector activeDirector;
+    private void Awake()
+    {
+        if (ins != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+            ins = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResumeTimeline();
+        }
+    }
+
+    public void SetDialogue(string lineOfDialogue, Sprite CharacterPhoto)
+    {
+        dialogueLineText.text = lineOfDialogue;
+        if (CharacterPhoto != null)
+        {
+            characterCG.sprite = CharacterPhoto;
+            characterCG.gameObject.SetActive(true);
+        }
+        else
+            characterCG = null;
+
+        dialogueLineText.gameObject.SetActive(true);
+        
+    }
+
+    //暂停TimeLine
+    public void PauseTimeline(PlayableDirector whichOne)
+    {
+        activeDirector = whichOne;
+
+        activeDirector.Pause();
+    }
+
+    //恢复播放TimeLine
+    public void ResumeTimeline()
+    {
+        dialogueLineText.gameObject.SetActive(false);
+        if(characterCG != null)
+            characterCG.gameObject.SetActive(false);
+        activeDirector.Resume();
+    }
+}
