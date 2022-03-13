f8f219e44bd3c306cc40d62332aa1108ba08db33
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-03-13 14:26:58 +0800

    electricDoor02

--- Assets/Script/Transition/TransitionDestination.cs
+++ Assets/Script/Transition/TransitionDestination.cs
@@ -7,7 +7,7 @@ public class TransitionDestination : MonoBehaviour
     public enum DestinationTag
     {
         FOREST,VILLEGE,VILLEGEFIRE, VILLEGEFIRE_TRAPTOSAVE, TRANSPORT_01,
-        TRANSPORT_01_2, TRANSPORT_02, TRANSPORT_02_2
+        TRANSPORT_01_2, TRANSPORT_02, TRANSPORT_02_2,DUNGEON,SEWER
     }
 
     public DestinationTag destinationTag;
