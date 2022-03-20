4db30299958e7d8658342c9f090b47cb15aed10d
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-03-20 17:32:48 +0800

    HealingUI

---
+++ Assets/Script/UI/FairyHealingUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyHealingUI : MonoBehaviour
+{
+    public GameObject healingUI1;
+    public GameObject healingUI2;
+    public GameObject healingUI3;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(GameManager.Instance.playerStats != null)
+        {
+            if (GameManager.Instance.playerStats.characterData.currentHealingTime == 0)
+            {
+                healingUI1.SetActive(false);
+                healingUI2.SetActive(false);
+                healingUI3.SetActive(false);
+            }else if(GameManager.Instance.playerStats.characterData.currentHealingTime == 1)
+            {
+                healingUI1.SetActive(true);
+                healingUI2.SetActive(false);
+                healingUI3.SetActive(false);
+            }else if(GameManager.Instance.playerStats.characterData.currentHealingTime == 2)
+            {
+                healingUI1.SetActive(true);
+                healingUI2.SetActive(true);
+                healingUI3.SetActive(false);
+            }else if(GameManager.Instance.playerStats.characterData.currentHealingTime == 3)
+            {
+                healingUI1.SetActive(true);
+                healingUI2.SetActive(true);
+                healingUI3.SetActive(true);
+            }
+                
+        }
+    }
+}
