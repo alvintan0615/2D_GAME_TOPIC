a4014b3ed9b8f37c724cadfcefd0e407b749bd76
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-03-30 17:46:24 +0800

    finalBoss01

---
+++ Assets/Script/FinalBoss/BossPosition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPosition : MonoBehaviour
+{
+    public Vector3 leftPos;
+    public Vector3 rightPos;
+    public float lerpSpeed;
+
+    [SerializeField] private bool touchLeft;
+    [SerializeField] private bool touchRight;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(transform.position.x == leftPos.x || transform.position.x < leftPos.x + 2f)
+        {
+            touchLeft = true;
+            touchRight = false;
+        }
+        if (transform.position.x == rightPos.x || transform.position.x > rightPos.x - 2f)
+        {
+            touchLeft = false;
+            touchRight = true;
+        }
+        if (touchLeft == true && touchRight == false)
+            transform.position = Vector2.Lerp(transform.position, rightPos, lerpSpeed * Time.deltaTime);
+        if (touchLeft == false && touchRight == true)
+            transform.position = Vector2.Lerp(transform.position, leftPos, lerpSpeed * Time.deltaTime);
+    }
+}
