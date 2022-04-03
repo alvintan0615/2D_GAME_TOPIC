7b6a6d93e3a4050d6b2374d11de497ab440904db
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-04-03 18:22:00 +0800

    SoundUpdate

---
+++ Assets/SoundEffect/Sound/MachineDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineDoor : MonoBehaviour
+{
+    public AudioSource MachineDoorSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MachineDoorSoundPlay()
+    {
+        MachineDoorSound.Play();
+    }
+}
