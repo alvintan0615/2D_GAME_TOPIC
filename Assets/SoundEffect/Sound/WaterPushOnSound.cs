781adb9211da6f02b486ce8f432caff0a9acf5fb
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-04-05 20:04:10 +0800

    UI ok

---
+++ Assets/SoundEffect/Sound/WaterPushOnSound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPushOnSound : MonoBehaviour
+{
+    public AudioSource WaterSound;
+    public bool canPlaySound = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canPlaySound = false;
+        WaterSound.mute = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canPlaySound == true)
+        {
+            WaterSound.mute = false;
+        }
+        else
+        {
+            WaterSound.mute = true;
+        }
+
+    }
+
+    public void WaterPushSound()
+    {
+        WaterSound.Play();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            canPlaySound = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            canPlaySound = false;
+        }
+    }
+}
