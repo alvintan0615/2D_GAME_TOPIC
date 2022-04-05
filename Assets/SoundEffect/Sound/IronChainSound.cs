f1cdcbcb771e495ba86b0c2adcaf3ab5a210c89a
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-04-05 21:34:54 +0800

    SoundUpdate

--- Assets/SoundEffect/Sound/IronChainSound.cs
+++ Assets/SoundEffect/Sound/IronChainSound.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class IronChainSound : MonoBehaviour
 {
     public AudioSource ironChain;
+   // public bool canPlaySound = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //canPlaySound = false;
+        //ironChain.mute = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if(canPlaySound == true)
+        //{
+        //    ironChain.mute = false;
+        //}
+        //else
+        //{
+        //    ironChain.mute = true;
+        //}
     }
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if(collision.tag == "Player")
+    //    {
+    //        canPlaySound = true; 
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if(collision.tag == "Player")
+    //    {
+    //        canPlaySound = false;
+    //    }
+    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
