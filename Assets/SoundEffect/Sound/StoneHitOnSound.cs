17f52fab6597419fa5e943b08351e25a7d92f37f
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-04-07 04:02:33 +0800

    soundUpdate

---
+++ Assets/SoundEffect/Sound/StoneHitOnSound.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneHitOnSound : MonoBehaviour
+{
+    public AudioSource StoneHit;
+    public bool canPlaySound = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canPlaySound = false;
+        StoneHit.mute = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canPlaySound == true)
+        {
+            StoneHit.mute = false;
+        }
+        else
+        {
+            StoneHit.mute = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            canPlaySound = true;
+            StoneHit.mute = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            canPlaySound = false;
+            StoneHit.mute = true;
+        }
+    }
+
+    public void StoneHitSound()
+    {
+        StoneHit.Play();
+    }
+}
