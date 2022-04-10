bfa421a55dafde1cb7338c84359fac74a480b55c
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-04-10 22:42:17 +0800

    ElectricDoorOK

---
+++ Assets/Script/Interactive/Dungeon/ElectricTimeLineTrick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class ElectricTimeLineTrick : MonoBehaviour
+{
+    public PlayableDirector mDirector;
+    public bool isTimeLineOK = false;
+    public GameObject light2d;
+    [SerializeField]private bool isOpen;
+    void Update()
+    {
+        if (isOpen == false)
+            light2d.SetActive(false);
+        else
+        {
+            light2d.SetActive(true);
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && EventManager.Instance.electricDoor == true && isTimeLineOK == false)
+        {
+            isOpen = true;
+            mDirector.Play();
+            isTimeLineOK = true;
+        }
+    }
+}
---
+++ Assets/Script/TimeLine/TimeLine_ElectricDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using System.Linq;
+
+public class TimeLine_ElectricDoor : MonoBehaviour
+{
+    public PlayableDirector mDirector;
+    public float normalizedTime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        normalizedTime = (float)(mDirector.time / mDirector.duration);
+
+        if (normalizedTime >= 0.01f && normalizedTime < 0.99f)
+        {
+            PlayerStatus.isDialouging = true;
+        }
+
+        if (normalizedTime >= 0.99f)
+        {
+            PlayerStatus.isDialouging = false;
+        }
+    }
+}
