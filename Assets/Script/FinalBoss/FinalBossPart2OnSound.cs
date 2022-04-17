722960fb6fbb8ab5dc38f6b7b43ba3c770ca629b
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-04-17 14:00:12 +0800

    BossUpdateSound

--- Assets/Script/FinalBoss/FinalBossPart2OnSound.cs
+++ Assets/Script/FinalBoss/FinalBossPart2OnSound.cs
@@ -22,5 +22,45 @@ public class FinalBossPart2OnSound : MonoBehaviour
         
     }
 
-   
+    public void PoisonAtkSound()
+    {
+        audioSetting.soundEffectAudio[19].Play();
+    }
+
+    public void PoisonExplosionSound()
+    {
+        audioSetting.soundEffectAudio[20].Play();
+    }
+
+    public void CloseAtkSound()
+    {
+        audioSetting.soundEffectAudio[21].Play();
+    }
+
+    public void KnockBackHitSound()
+    {
+        audioSetting.soundEffectAudio[22].Play();
+    }
+
+    public void CreateEletricBallSound()
+    {
+        audioSetting.soundEffectAudio[23].mute = false;
+        audioSetting.soundEffectAudio[23].Play();
+    }
+
+    public void MuteCreateEletricBallSound()
+    {
+        StartCoroutine(DelayMuteCreateEletricBallSound());
+    }
+
+    public void ThrowEletricBallSound()
+    {
+        audioSetting.soundEffectAudio[24].Play();
+    }
+
+    IEnumerator DelayMuteCreateEletricBallSound()
+    {
+        yield return new WaitForSeconds(1);
+        audioSetting.soundEffectAudio[23].mute = true;
+    }
 }
