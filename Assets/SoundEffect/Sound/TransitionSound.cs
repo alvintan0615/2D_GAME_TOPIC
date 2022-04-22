6fc650e269b3360e4df014827b7c943f15ebccf2
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-04-23 01:15:00 +0800

    SoundUpdate

--- Assets/SoundEffect/Sound/TransitionSound.cs
+++ Assets/SoundEffect/Sound/TransitionSound.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class TransitionSound : MonoBehaviour
 {
-
+    public AudioSetting audioSetting;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSetting = GameObject.Find("SFX").GetComponent<AudioSetting>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            audioSetting.soundEffectAudio[34].Play();
+        }
     }
 }
