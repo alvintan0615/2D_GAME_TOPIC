0abad952c019f73afddd4a68851c447fc8948041
alvintan0615 <joonyyangtan1999@gmail.coom>
2022-04-25 03:15:02 +0800

    SoundUpdate

---
+++ Assets/SoundEffect/Sound/BossKeyGetSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKeyGetSound : MonoBehaviour
+{
+    public AudioSetting audioSetting;
+    static public bool KeyHasGet = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSetting = GameObject.Find("SFX").GetComponent<AudioSetting>();
+        KeyHasGet = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audioSetting.soundEffectAudio[35].Play();
+            audioSetting.soundEffectAudio[36].mute = true;
+            KeyHasGet = true;
+        }
+    }
+}
---
+++ Assets/SoundEffect/Sound/KeySound.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySound : MonoBehaviour
+{
+    public AudioSetting audioSetting;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSetting = GameObject.Find("SFX").GetComponent<AudioSetting>();
+        audioSetting.soundEffectAudio[36].mute = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && BossKeyGetSound.KeyHasGet == false)
+        {
+            audioSetting.soundEffectAudio[36].mute = false;
+        }
+
+        if (collision.tag == "Player" && BossKeyGetSound.KeyHasGet == true)
+        {
+            audioSetting.soundEffectAudio[36].mute = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && BossKeyGetSound.KeyHasGet == false)
+        {
+            audioSetting.soundEffectAudio[36].mute = true;
+        }
+
+        if (collision.tag == "Player" && BossKeyGetSound.KeyHasGet == true)
+        {
+            audioSetting.soundEffectAudio[36].mute = true;
+        }
+    }
+
+    public void KeySoundUnmuteForTimeLine()
+    {
+        audioSetting.soundEffectAudio[37].Play();
+        audioSetting.soundEffectAudio[37].mute = false;
+    }
+
+    public void KeySoundMuteForTimeLine()
+    {
+        audioSetting.soundEffectAudio[37].mute = true;
+    }
+
+}
