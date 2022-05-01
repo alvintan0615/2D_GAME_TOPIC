9dc3b933a5e3a4142b494e7b4497d2fc95ba3049
az22564785 <71372783+az22564785@users.noreply.github.com>
2022-05-01 12:54:16 +0800

    fix

---
+++ Assets/Script/CineMachineFollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CineMachineFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera followCamera;
+    [SerializeField]private float timer;
+    [SerializeField] private bool isOK = false;
+    private void Start()
+    {
+        followCamera = GetComponent<CinemachineVirtualCamera>();
+        if(GameManager.Instance.playerStats != null)
+        {
+            followCamera.Follow = GameManager.Instance.playerStats.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if(isOK == false)
+        {
+            followCamera.Follow = GameManager.Instance.playerStats.transform;
+            timer += Time.deltaTime;
+            if (timer >= 3f)
+            {
+                isOK = true;
+                timer = 0f;
+
+            }
+        }
+
+    }
+}
